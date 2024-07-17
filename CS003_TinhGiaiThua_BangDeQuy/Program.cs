d5adc9482bec8c0569308084a516ef8b79902167
hoangngoanngoan <ldh2381995@gmail.com>
2024-07-18 00:32:27 +0700

    Add project files.

---
+++ CS003_TinhGiaiThua_BangDeQuy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.Design;
+namespace CS003_TinhGiaiThua_BangDeQuy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; 
+            Console.WriteLine("Chương trình tính giai thừa bằng đệ quy");
+            int a;
+            Console.Write("Nhấp số: "); a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Kết quả của chương trình là: {TinhGT(a)}");
+        }
+        static int TinhGT(int a)
+        {
+            int ketqua;
+            ketqua = a;
+            if (a == 0) return ketqua = 1;
+            else ketqua *= TinhGT(a - 1);
+            return ketqua;
+        }
+    }
+}
